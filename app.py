60225ab7fad5d489f58d2cf1faa6b307ad3105a1
Rachel <102566199+ibachrachel@users.noreply.github.com>
2022-06-21 19:04:20 -0700

    Add files via upload
    most recent versions

--- app.py
+++ app.py
@@ -1,30 +1,35 @@
-#import tools: 
-
+# Import flask, PyMongo, and scraping code
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
-# set up flask
+# Set up flask
 app = Flask(__name__)
 
-#tell python how to connect to mongo via pymongo
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-#define the route
+# Home route
 @app.route("/")
 def index():
-   mars = mongo.db.mars.find_one()
-   return render_template("index.html", mars=mars)
+    # Uses PyMongo to find the mars collection in our database
+    mars = mongo.db.mars.find_one()
+    # tells Flask to return html template and use mars collection
+    return render_template("index.html", mars=mars)
 
-#define next route and function: 
+# Route will scrape updated data
 @app.route("/scrape")
 def scrape():
-   mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
-   mars.update_one({}, {"$set":mars_data}, upsert=True)
-   return redirect('/', code=302)
+    # variable points to mongo db
+    mars = mongo.db.mars
+    # holds newly scraped data
+    mars_data = scraping.scrape_all()
+    # insert data into mongo db
+    mars.update_one({}, {"$set":mars_data}, upsert=True)
+    # navigate back to homepage
+    return redirect('/', code=302)
 
+# Tell flask to run
 if __name__ == "__main__":
    app.run()
